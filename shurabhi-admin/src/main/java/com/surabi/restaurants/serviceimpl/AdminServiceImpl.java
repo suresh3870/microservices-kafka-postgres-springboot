f99afcdd8d18e2f289326091d62ed8383699be95
suresh3870 <sksureshkumar88@gmail.com>
2022-01-16 02:55:44 +0530

    Fix bill query

--- shurabhi-admin/src/main/java/com/surabi/restaurants/serviceimpl/AdminServiceImpl.java
+++ shurabhi-admin/src/main/java/com/surabi/restaurants/serviceimpl/AdminServiceImpl.java
@@ -116,7 +116,7 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public List<BillDTO> viewMyBills(String userName) {
         Query nativeQuery = entityManager.createNativeQuery("select distinct b.BILLID as BILL_ID,  u.USERNAME as USERNAME, b.BILL_DATE as BILL_DATE ,b.BILL_AMOUNT as BILL_AMOUNT from  users u , BILL b, ORDERS o where  u.USERNAME=o.USERNAME  \n" +
-                "and o.username=u.username and o.username=?1","BillViewMapping" );
+                "and o.username=u.username and o.order_id=b.order_id and o.username=?1","BillViewMapping" );
         nativeQuery.setParameter(1, userName);
         List<BillDTO> list =  nativeQuery.getResultList();
         return list;
